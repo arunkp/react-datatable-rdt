c4acc32e6cfc6c2311ebd8300da0a8b8ed65137f
Arun Rao <arun.kalya.pranesh.rao@pwc.com>
2023-02-24 18:53:48 +0100

    fix for exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,7 @@
-export function sayHello(name: string) {
-  return `Hey ${name}, say hello to TypeScript.`;
-}
+import React from 'react';
+
+const SayHello = ({ name }: { name: string }): JSX.Element => (
+  <div>Hey {name}, go hello to TypeScript.</div>
+);
+
+export default SayHello;
